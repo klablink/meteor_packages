d77a2a886867e0c89225334d4639734cbca6f817
Pierluigi Beato <p.beato@lectra.com>
2024-03-18 09:07:05 +0100

    fix(CORE): [CORE] Fix socket-stream-client

---
+++ socket-stream-client/server.js
@@ -0,0 +1,22 @@
+import {
+  setMinimumBrowserVersions,
+} from "meteor/modern-browsers";
+
+setMinimumBrowserVersions({
+  chrome: 16,
+  edge: 12,
+  firefox: 11,
+  ie: 10,
+  mobileSafari: [6, 1],
+  phantomjs: 2,
+  safari: 7,
+  electron: [0, 20],
+}, module.id);
+
+if (process.env.DISABLE_SOCKJS) {
+  __meteor_runtime_config__.DISABLE_SOCKJS = process.env.DISABLE_SOCKJS;
+}
+
+__meteor_runtime_config__._sockjsOptions = {
+    timeout: process.env.SOCKJS_TIMEOUT || 0,
+}
