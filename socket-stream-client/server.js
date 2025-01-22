546c108c37b3f90af9fc42e917670496a8019b25
d.ruggeroni <d.ruggeroni@lectra.com>
2025-01-22 10:04:09 +0100

    update to version 0.6.0

--- socket-stream-client/server.js
+++ socket-stream-client/server.js
@@ -1,20 +1,20 @@
 import {
-  setMinimumBrowserVersions,
+    setMinimumBrowserVersions,
 } from "meteor/modern-browsers";
 
 setMinimumBrowserVersions({
-  chrome: 16,
-  edge: 12,
-  firefox: 11,
-  ie: 10,
-  mobileSafari: [6, 1],
-  phantomjs: 2,
-  safari: 7,
-  electron: [0, 20],
+    chrome: 16,
+    edge: 12,
+    firefox: 11,
+    ie: 10,
+    mobileSafari: [6, 1],
+    phantomjs: 2,
+    safari: 7,
+    electron: [0, 20],
 }, module.id);
 
 if (process.env.DISABLE_SOCKJS) {
-  __meteor_runtime_config__.DISABLE_SOCKJS = process.env.DISABLE_SOCKJS;
+    __meteor_runtime_config__.DISABLE_SOCKJS = process.env.DISABLE_SOCKJS;
 }
 
 __meteor_runtime_config__._sockjsOptions = {
